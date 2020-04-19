2f22508d70c0088fbca9f7549e6521999f62207b
evgestrogan <evgestrogan@gmail.com>
2020-04-19 20:08:24 +0300

    Create all function in main menu. Now my application can create matrix, solve nonogram, save this nonogram and open saved nonogram. I'm using .json format file for saving data. I'm add file 30x30.txt which implement big matrix with value and solved matrix.

--- Conversion_matrix_to_PNG.py
+++ Conversion_matrix_to_PNG.py
@@ -3,13 +3,14 @@ from PIL import Image, ImageDraw
 
 class Converter:
     def __init__(self, matrix):
-        self._matrix = matrix
-        self._img = Image.new('RGB', (len(matrix), len(matrix[0])), color=(255, 255, 255))
-        self._d = ImageDraw.Draw(self._img)
+        self.matrix = matrix
+        self.img = Image.new('RGB', (len(matrix), len(matrix[0])), color=(255, 255, 255))
+        self.d = ImageDraw.Draw(self.img)
+        self.create_points()
 
     def create_points(self):
-        for y, row in enumerate(self._matrix):
+        for y, row in enumerate(self.matrix):
             for x, numb in enumerate(row):
                 if numb == 2:
-                    self._d.point([x, y], fill='red')
-        self._img.save('result.png')
+                    self.d.point([x, y], fill='black')
+        self.img.save('result.png')
--- Main_window.py
+++ Main_window.py
@@ -6,31 +6,20 @@ from Nonogram_solver import Solver
 
 class MainUI(Tk):
 
-    def take_size_rectangle(self, entry, window):
-        self.size_rectangle = entry.get()
-        self.create_matrix_frame()
-        window.destroy()
-
-    def create_second_window(self):
-        window = Toplevel(self)
-        data = IntVar()
-        Entry(window, textvariable=data).grid(column=0, row=0)
-        Button(window, text='Input size', command=lambda: self.take_size_rectangle(data, window)).grid(column=1, row=0)
-        window.mainloop()
-
     def __init__(self):
         Tk.__init__(self)
         self.resizable(height=False, width=False)
-        self.size_rectangle = 0
+        self.filename = ''
         self.side_length = 22
+        self.result_matrix = []
 
         """Menu bar"""
         menubar = Menu(self)
         filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Create work window", command=self.create_second_window)
-        filemenu.add_command(label="Solver nonogram", command=self.get_values)
-        filemenu.add_command(label="Save nonogram", command=self.create_matrix_frame)
-        filemenu.add_command(label="Open saved nonogram", command=self.create_matrix_frame)
+        filemenu.add_command(label="Create work window", command=lambda: self.create_second_window('Input size'))
+        filemenu.add_command(label="Solver nonogram", command=self.solution_nonogram)
+        filemenu.add_command(label="Save nonogram", command=lambda: self.create_second_window('Input file name'))
+        filemenu.add_command(label="Open nonogram", command=lambda: self.create_second_window('Select file'))
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.destroy)
         menubar.add_cascade(label="File", menu=filemenu)
@@ -40,18 +29,89 @@ class MainUI(Tk):
         self.container = Frame(self, width=100, height=100)
         self.container.pack(fill=BOTH, expand=True)
 
-    def create_matrix_frame(self):
-        self.x_value = InputFrame(self.container, self.size_rectangle, 'top')
-        self.x_value.grid(row=0, column=1, sticky="nsew")
-        self.y_value = InputFrame(self.container, self.size_rectangle, 'left')
-        self.y_value.grid(row=1, column=0, sticky="nsew")
-        MatrixFrame(self.container, self.size_rectangle, self.side_length).grid(row=1, column=1, sticky="nsew")
+        self.square_matrix = None
+        self.y_value = None
+        self.x_value = None
 
-    def get_values(self):
-        result_matrix = Solver(self.x_value.take_input_value(), self.y_value.take_input_value())
-        result_matrix.solve()
-        convert_matrix = Converter(result_matrix.zero_matrix)
-        convert_matrix.create_points()
+    def take_input_value(self, entry, window, text):
+        input_value = entry.get()
+        if text == 'Input size':
+            self.result_matrix = [[0] * int(input_value) for i in range(int(input_value))]
+            self.create_matrix_frame()
+        elif text == 'Input file name':
+            self.filename = input_value + '.txt'
+            self.save_values()
+        elif text == 'Select file':
+            self.filename = input_value
+            self.open_nonogram()
+        window.destroy()
+
+    def create_second_window(self, text):
+        import os
+        window = Toplevel(self)
+        data = Entry(window)
+        data.grid(column=0, row=0)
+        Button(window, text=text, command=lambda: self.take_input_value(data, window, text)).grid(column=1, row=0)
+        if text == 'Select file':
+            def set_text(event):
+                data.delete(0, END)
+                data.insert(0, lbox.get(lbox.curselection()[0]))
+                return
+
+            lbox = Listbox(window)
+            lbox.grid(row=2, columnspan=2)
+            for item in os.listdir():
+                if item.endswith('.txt'):
+                    lbox.insert(END, item)
+            lbox.bind("<<ListboxSelect>>", set_text)
+        window.mainloop()
+
+    def create_matrix_frame(self):
+        size_rectangle = len(self.result_matrix)
+        self.square_matrix = MatrixFrame(self.container, size_rectangle, self.side_length)
+        self.y_value = InputFrame(self.container, size_rectangle, 'left')
+        self.x_value = InputFrame(self.container, size_rectangle, 'top')
+        self.x_value.grid(row=0, column=1, sticky="nsew")
+        self.y_value.grid(row=1, column=0, sticky="nsew")
+        self.square_matrix.grid(row=1, column=1, sticky="nsew")
+
+    def solution_nonogram(self):
+        solver = Solver([self.x_value.take_input_value(), self.y_value.take_input_value()], self.result_matrix)
+        self.result_matrix = solver.zero_matrix
+        Converter(self.result_matrix)
+        self.print_square()
+
+    def print_square(self):
+        for itemls_valuels in zip(self.result_matrix, self.square_matrix.square_list):
+            for item_value in zip(itemls_valuels[0], itemls_valuels[1]):
+                if item_value[0] == 2:
+                    item_value[1].move(None)
+                elif item_value[0] == 1:
+                    item_value[1].clear()
+
+    def save_values(self):
+        import json
+        self.create_result_matrix()
+        with open(self.filename, 'w') as fp:
+            json.dump([self.x_value.take_input_value(), self.y_value.take_input_value(), self.result_matrix], fp)
+
+    def create_result_matrix(self):
+        for x, row in enumerate(self.square_matrix.square_list):
+            for y, obj in enumerate(row):
+                self.result_matrix[x][y] = obj.button_state
+
+    def open_nonogram(self):
+        import json
+        with open(self.filename, 'r') as fp:
+            x_val, y_val, self.result_matrix = json.load(fp)
+        self.create_matrix_frame()
+        self.print_square()
+        for item_value in zip(x_val, self.x_value.list_entry):
+            for item in item_value[0]:
+                item_value[1].insert(END, str(item) + '\n')
+        for item_value in zip(y_val, self.y_value.list_entry):
+            item_value[1].delete(END, END)
+            item_value[1].insert(END, item_value[0])
 
 class MatrixFrame(Frame):
 
@@ -60,9 +120,11 @@ class MatrixFrame(Frame):
         start_x = start_y = 0
         self.canvas = Canvas(self, width=side_length * size_rectangle,
                              height=side_length * size_rectangle)
+        self.square_list = []
         for row in range(size_rectangle):
+            self.square_list.append([])
             for column in range(size_rectangle):
-                CreateCanvasSquare(self.canvas, start_x, start_y, side_length, 'white')
+                self.square_list[row].append(CreateCanvasSquare(self.canvas, start_x, start_y, side_length, 'white'))
                 start_x += side_length
             start_y += side_length
             start_x = 0
@@ -91,9 +153,14 @@ class CreateCanvasSquare:
         self.square = self.master.create_rectangle(start_x, start_y, start_x + size_square, start_y + size_square,
                                                    fill=color)
         self.master.tag_bind(self.square, '<Button-1>', self.move)
+        self.button_state = 1
 
     def move(self, event):
-        self.master.itemconfig(self.square, fill='red')
+        self.master.itemconfig(self.square, fill='black')
+        self.button_state = 2
+
+    def clear(self):
+        self.master.itemconfig(self.square, fill='white')
 
 
 class CreateInput(Text):
@@ -107,5 +174,6 @@ class CreateInput(Text):
         return [int(s) for s in self.get("1.0", END).split() if s.isdigit()]
 
 
-MainUIobj = MainUI()
-MainUIobj.mainloop()
+if __name__ == "__main__":
+    MainUIobj = MainUI()
+    MainUIobj.mainloop()
